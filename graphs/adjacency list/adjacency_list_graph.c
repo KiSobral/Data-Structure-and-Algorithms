65c4a18ed732385640e92d2f4f07c72e87314c19
Hugo Sobral <hugosobral10@gmail.com>
2020-03-19 18:55:28 -0300

    fix the C code style

--- graphs/adjacency list/adjacency_list_graph.c
+++ graphs/adjacency list/adjacency_list_graph.c
@@ -46,39 +46,3 @@ void addEdge(struct Graph* graph, int from, int dest) {
     new_node->next = graph->array[dest].head; 
     graph->array[dest].head = new_node; 
 } 
-
-void printGraph(struct Graph* graph) 
-{ 
-    int v; 
-    for (v = 0; v < graph->num_of_edges; v++) 
-    { 
-        struct AdjListNode* pCrawl = graph->array[v].head; 
-        printf("\n Adjacency list of vertex %d\n head ", v); 
-        while (pCrawl) 
-        { 
-            printf("-> %d", pCrawl->dest); 
-            pCrawl = pCrawl->next; 
-        } 
-        printf("\n"); 
-    } 
-} 
-  
-// Driver program to test above functions 
-int main() 
-{ 
-    // create the graph given in above fugure 
-    int V = 5; 
-    struct Graph* graph = createGraph(V); 
-    addEdge(graph, 0, 1); 
-    addEdge(graph, 0, 4); 
-    addEdge(graph, 1, 2); 
-    addEdge(graph, 1, 3); 
-    addEdge(graph, 1, 4); 
-    addEdge(graph, 2, 3); 
-    addEdge(graph, 3, 4); 
-  
-    // print the adjacency list representation of the above graph 
-    printGraph(graph); 
-  
-    return 0; 
-} 
