419070a720ffb5c966357e608cf844b18568c78f
Hugo Sobral <hugosobral10@gmail.com>
2021-02-10 14:05:28 -0300

    Update c implementation of insertion sort

--- sorts/insertion_sort/c/insertion_sort.c
+++ sorts/insertion_sort/c/insertion_sort.c
@@ -1,42 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void insertionSort(int vetor[], int n) { 
-    int i; 
-    int j;
-    int valor;
-    
-    for (i = 1; i < n; i++) { 
-        valor = vetor[i]; 
-  
-        for (j=i-1; j>=0 && vetor[j]>valor; j--) vetor[j+1] = vetor[j];
+#define comp(A,B) ((A) < (B))
+#define exch(A,B) { int t=A; A=B; B=t; }
+#define cmpexch(A,B) { if(comp(B,A)) exch(A,B); }
 
-        vetor[j + 1] = valor; 
+typedef int Item;
+
+void insertionSort(Item *v, int l, int r){
+    for (int i=r; i>l; i--)
+        cmpexch(v[i-1], v[i]);
+
+    for (int i=l+2; i<=r; i++){
+        int j=i;
+        Item tmp=v[j];
+
+        while(comp(tmp, v[j-1])){
+            v[j]=v[j-1];
+            j--;
+        }
+
+        v[j] = tmp;
     }
 }
 
-void recursiveInsertionSort(int vetor[], int n) {
-    if (n<=1) return;
-
-    recursiveInsertionSort(vetor, n-1);
-
-    int valor = vetor[n-1];
-    int j;
-
-    for (j=n-2; j>=0 && vetor[j]>valor; j--) vetor[j+1] = vetor[j];
-
-    vetor[j+1] = valor;
+void dummyInsertionSort(Item *v,int l,int r){
+  for(int i=l+1;i<=r;i++)
+    for(int j=i;j>l;j--)
+        cmpexch(v[j-1],v[j]);
 }
 
+
 int main() {
-    int vetor[5] = {5, 4, 3, 1, 2};
+    Item *v;
 
-    for (int i=0; i<5; i++) printf("%d ", vetor[i]);
+    v = malloc(5*sizeof(Item));
+
+    v[0]=5; v[1]=2; v[2]=3; v[3]=1; v[4]=4;
+
+    printf("Vetor antes da ordenação: ");
+    for (int i=0; i<5; i++) printf("%d ", v[i]);
     printf("\n");
 
-    recursiveInsertionSort(vetor, 5);
-
-    for (int i=0; i<5; i++) printf("%d ", vetor[i]);
+    insertionSort(v, 0, 4);
+    printf("Vetor depois da ordenação: ");
+    for (int i=0; i<5; i++) printf("%d ", v[i]);
     printf("\n");
 
+    v[0]=5; v[1]=2; v[2]=3; v[3]=1; v[4]=4;
+
+    dummyInsertionSort(v, 0, 4);
+    printf("Vetor depois da ordenação lenta: ");
+    for (int i=0; i<5; i++) printf("%d ", v[i]);
+    printf("\n");
+
+
+    free(v);
     return 0;
 }
