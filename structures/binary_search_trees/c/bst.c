80999fffdad4d56f403fca6c2bce27bd2a7729a8
Hugo Sobral <hugosobral10@gmail.com>
2021-03-30 12:19:57 -0300

    Include delete method for binary tree nodes

--- structures/binary_search_trees/c/bst.c
+++ structures/binary_search_trees/c/bst.c
@@ -38,6 +38,64 @@ link insert(link root, item x) {
     return root;
 }
 
+link search(link root, item x){
+    if (root == NULL)   return NULL;
+    if (root->val == x) return root;
+    if (x < root->val)  return search(root->left, x);
+    if (x > root->val)  return search(root->right, x);
+}
+
+link aux_search(link root, item x, link *father){
+    link current = root;
+    *father = NULL;
+
+    while (current){
+        if (current->val == x) return current;
+        *father = current;
+
+        if (x < current->val) current = current->left;
+        else                  current = current->right;
+    }
+
+    return NULL;
+}
+
+link remove_node(link root, item x){
+    link father, node, sub_father, substitute;
+    node = aux_search(root, x, &father);
+    if (node == NULL) return root;
+    
+    if (!node->left || !node->right){
+        if (!node->left) substitute = node->right;
+        else             substitute = node->left;
+    } else {
+        sub_father = node;
+        substitute = node->left;
+        while (substitute->right){
+            sub_father = substitute;
+            substitute = substitute->right;
+        }
+
+        if (sub_father != node){
+            sub_father->right = substitute->left;
+            substitute->left  = node->left;
+        }
+
+        substitute->right = node->right;
+
+        if (!father) {
+            free(node);
+            return substitute;
+        }
+
+        if (x < father->val) father->left = substitute;
+        else                 father->right = substitute;
+
+        free(node);
+        return(root);
+    }
+}
+
 void pre_order(link root){
     if (root==NULL) return;
     printf("%d ", root->val);
@@ -46,23 +104,36 @@ void pre_order(link root){
 }
 void in_order(link root){
     if (root==NULL) return;
-    pre_order(root->left);
+    in_order(root->left);
     printf("%d ", root->val);
-    pre_order(root->right);
+    in_order(root->right);
 }
 void post_order(link root){
     if (root==NULL) return;
-    pre_order(root->left);
-    pre_order(root->right);
+    post_order(root->left);
+    post_order(root->right);
     printf("%d ", root->val);
 }
 
 int main(){
     link root = initialize();
-    root = insert(root, 23);
-    root = insert(root, 24);
-    root = insert(root, 22);
+    root = insert(root, 18);
+    root = insert(root, 8);
+    root = insert(root, 2);
+    root = insert(root, 14);
+    root = insert(root, 10);
+    root = insert(root, 16);
+    root = insert(root, 9);
+    root = insert(root, 12);
+    root = insert(root, 11);
 
-    post_order(root);
+    in_order(root);
     printf("\n");
+
+    root = remove_node(root, 14);
+
+    in_order(root);
+    printf("\n");
+
+    return 0;
 }
